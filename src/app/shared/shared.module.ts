baf31aa3abf77a7c1534f75024d3760efa3af540
Dóka Balázs <dbalazs40@gmail.com>
2020-07-06 18:33:17 +0200

    Add header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [ HeaderComponent ],
   imports: [
     CommonModule,
+  ],
+  exports: [
+    HeaderComponent,
   ],
 })
 export class SharedModule {
